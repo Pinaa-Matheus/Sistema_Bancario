523e3419876b311479278474bee32bfb668a6caa
Matheus Pina <m.pinadev@gmail.com>
2025-06-21 18:33:04 -0300

    Adicionado a opção deposito

--- Sistema_Bancario.py
+++ Sistema_Bancario.py
@@ -19,11 +19,17 @@ while True:
     opcao = input(MENU)
 
     if opcao == "1":
-        print("Depósito selecionado")
+        deposito = int(input("Valor do deposito"))
+        if deposito > 0:
+            deposito += saldo
+            print(f"Realizando o deposito de {deposito}")
+            extrato += f"\nDeposito realizado: R$ {deposito}\n" #Add o extrato, o \n é para não ficar desorganizado.
+        else: #Adicionar uma mensagem para 0 reais
+            print("Não foi possivel realizar seu deposito")
     elif opcao == "2":
         print("Saque selecionado")
     elif opcao == "3":
-        print("Extrato selecionado")
+        print(extrato)
     elif opcao == "4":
         print("Saindo...")
         break
