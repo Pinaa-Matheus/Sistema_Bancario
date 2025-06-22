883219fca9c829af92d384118712e8f200caf770
Matheus Pina <m.pinadev@gmail.com>
2025-06-22 02:49:16 -0300

    Adicionado a opção saque

--- Sistema_Bancario.py
+++ Sistema_Bancario.py
@@ -19,19 +19,35 @@ while True:
     opcao = input(MENU)
 
     if opcao == "1":
-        deposito = int(input("Valor do deposito"))
+        deposito = float(input("Valor do deposito R$: "))
         if deposito > 0:
-            deposito += saldo
-            print(f"Realizando o deposito de {deposito}")
+            saldo += deposito
+            print(f"Realizando o deposito de {deposito}!")
             extrato += f"\nDeposito realizado: R$ {deposito}\n" #Add o extrato, o \n é para não ficar desorganizado.
-        else: #Adicionar uma mensagem para 0 reais
-            print("Não foi possivel realizar seu deposito")
+        else: 
+            print("Não foi possivel realizar seu deposito!")
+
     elif opcao == "2":
-        print("Saque selecionado")
+            saque = float(input("Valor do Saque R$: "))
+            if saque <= 0: #Ver se o valor é posivo
+                print("Operação falhou! O valor informado para o saque é inválido. Por favor, insira um valor positivo.")
+            elif numero_saques >= LIMITE_SAQUES: #Ver se ele atingiu o limite diario
+                print("Operação falhou! Você atingiu o limite diário de saques.")          
+            elif saque > limite: #Ver se ele tem limite
+                print(f"Operação falhou! Voce atingiu o seu limite de {limite}")
+            elif saque > saldo: #Ver se ele tem saldo
+                print("Operação falhou! Você não tem saldo suficiente")
+            else:
+                saldo -= saque #Diminui o valor do saque do saldo
+                numero_saques += 1 
+                extrato += f"\nSaque Realizado: {saque}\n"
+                print(f"Saque de R$ {saque} realizado!")
+
     elif opcao == "3":
-        print(extrato)
+            print(extrato)
+
     elif opcao == "4":
-        print("Saindo...")
-        break
+            print("Saindo...")
+            break
     else:
         print("Operação Inválida")
