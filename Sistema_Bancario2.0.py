f6e3353bdd5a79dbe6a54e5343bdf51615988c36
Matheus Pina <m.pinadev@gmail.com>
2025-07-16 15:53:11 -0300

    Add sistema bancario 2.0

---
+++ Sistema_Bancario2.0.py
@@ -0,0 +1,47 @@
+#### Variaveis ####
+cliente = []
+
+saldo = 0
+limite = 500
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 3
+
+####################
+MENU_CADASTRO = """"
+========= Cadastro =========
+
+       [1] Depositar
+       [2] Depositar
+
+============================
+"""
+
+def cadastrar_usuario():
+    nome = input("Nome: ").strip() #Strip para não ter espaços 
+
+
+cpf = input("CPF (somente números): ").strip()
+cpf = ''d.join
+#checar se tem letra com true/false com isalpha()
+#ve se tem 11 digitos 
+
+
+
+MENU = f("""
+========= MENU =========
+  Bem-Vindo {cliente}
+
+    [1] Depositar
+    [2] Sacar
+    [3] Extrato
+    [4] Sair
+
+========================
+""")
+
+
+
+#Cadastrar novo usuario 
+#Cadastrar nova conta no banco 
+#Criar funções para todo o sistema bancario 
