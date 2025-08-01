d82b40107ffc8c07fca75f37f15aa529375cbdc9
Matheus Pina <m.pinadev@gmail.com>
2025-08-01 12:32:43 -0300

    Att no cadastro

--- Sistema_Bancario2.0.py
+++ Sistema_Bancario2.0.py
@@ -9,29 +9,50 @@ LIMITE_SAQUES = 3
 
 ####################
 
-MENU_CADASTRO = """"
+MENU_CADASTRO = """
 ========= Cadastro =========
 
        [1] Cadastra
        [2] Depositar
+       [0] Sair
 
 ============================
 """
 
 
-while True:
-    opcao = input(MENU_CADASTRO)
+def cadastrar_usuario():
+        nome = input("Nome: ").strip() #Strip para não ter espaços 
 
+        cpf = input("CPF (somente números): ").strip()
+        cpf = ''.join(filter(str.isdigit, cpf)) # Filter vai passar por todas os caracteres
 
+        if len(cpf) != 11:
+                print("CPF inválido! Contate nosso suporte.")
+                return
 
-def cadastrar_usuario(opcao):
-    nome = input("Nome: ").strip() #Strip para não ter espaços 
-
-cpf = input("CPF (somente números): ").strip()
-cpf = ''.join(filter(str.isdigit, cpf)) # Filter vai passar por todas os caracteres
+        for usuario in usuarios:
+            if usuario["cpf"] == cpf:
+                print("\n Já existe um usuário cadastrado com esse CPF!")
+                return
 #checar se tem letra com true/false com isalpha()
 #ve se tem 11 digitos 
 
+
+while True:
+    opcao = input(MENU_CADASTRO).strip()
+
+    if opcao == "1":
+        cadastrar_usuario()
+
+    elif opcao == "2":
+        
+
+    elif opcao == "0":
+        print("Saindo do sistema...")
+        break
+
+    else:
+        print("Opção inválida, tente novamente.")
 
 
 MENU = f("""
@@ -45,9 +66,6 @@ MENU = f("""
 
 ========================
 """)
-
-
-
 #Cadastrar novo usuario 
 #Cadastrar nova conta no banco 
 #Criar funções para todo o sistema bancario 
